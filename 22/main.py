538e2b237f048759736e9a9f04d5762d0574799b
Chris Stewart <mils8545@gmail.com>
2022-09-24 13:03:28 -0600

    Day 22: Brute Force, Takes ay too long.

--- 22/main.py
+++ 22/main.py
@@ -13,17 +13,75 @@ def readFile(fileName : str) -> List[str]:
         lines[i] = lines[i].rstrip()
     return lines
 
+class Point:
+    def __init__(self, x : int, y : int) -> None:
+        self.x : int = x
+        self.y : int = y
+    def __str__(self) -> str:
+        return f"({self.x},{self.y})"
+    def __add__(self, other) -> "Point":
+        return Point(self.x + other.x, self.y + other.y)
+
+def parseLines(lines : List[str]) -> List[str]:
+    width : int = len(lines[0])
+    height : int = len(lines)
+    infected : List[str] = []
+    for y in range(len(lines)):
+        for x in range(len(lines[y])):
+            if lines[y][x] == "#":
+                infected.append(str(Point(x - width // 2, y - height // 2)))
+    return infected
+
 def part1(lines : List[str]) -> str:
     # Code the solution to part 1 here, returning the answer as a string
+    infected : List[str] = parseLines(lines)
+    DIRECTIONS : List[Point] = [Point(0, -1), Point(1, 0), Point(0, 1), Point(-1, 0)]
+    direction : int = 0
+    position : Point = Point(0, 0)
+    count : int = 0
 
-    return f"Not Implemented"
+    for i in range(10000):
+        if str(position) in infected:
+            direction = (direction + 1) % 4
+            infected.remove(str(position))
+        else:
+            direction = (direction - 1) % 4
+            infected.append(str(position))
+            count += 1
+        position += DIRECTIONS[direction]
+    return f"There were {count} infections after 10,000 cycles."
 
 def part2(lines : List[str]) -> str:
     # Code the solution to part 2 here, returning the answer as a string
-    
-    
-    return f"Not Implemented"
-    pass
+    infected : List[str] = parseLines(lines)
+    weakened : List[str] = []
+    flagged : List[str] = []
+    DIRECTIONS : List[Point] = [Point(0, -1), Point(1, 0), Point(0, 1), Point(-1, 0)]
+    direction : int = 0
+    position : Point = Point(0, 0)
+    count : int = 0
+
+    for i in range(10000000):
+    # for i in range(100):
+        # if i % 100000 == 0:
+        #     print(i)
+        if str(position) in infected:
+            direction = (direction + 1) % 4
+            flagged.append(str(position))
+            infected.remove(str(position))
+        elif str(position) in weakened:
+            weakened.remove(str(position))
+            infected.append(str(position))
+            count += 1
+        elif str(position) in flagged:
+            direction = (direction + 2) % 4
+            flagged.remove(str(position))
+        else:
+            weakened.append(str(position))
+            direction = (direction - 1) % 4
+
+        position += DIRECTIONS[direction]
+    return f"There were {count} infections after 10,000,000 cycles."
 
 def main () -> None:
     # Opens a dialog to select the input file
